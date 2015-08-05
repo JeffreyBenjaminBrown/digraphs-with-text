1a093f4d5d951a2e213057de5a4378c890a05409
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2015-08-05 13:05:17 -0700

    trivial

---
+++ other/hand-imports.hs
@@ -0,0 +1,6 @@
+import Control.Lens (makeLenses, view, (^.), (^?), over, (%~), set, (.~) )
+import qualified Data.Set as Set
+import qualified Data.Map as Map
+import qualified Data.Maybe as Maybe
+import Control.Exception (assert)
+
