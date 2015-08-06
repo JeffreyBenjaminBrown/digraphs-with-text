2088a951755fd50cd2ef47bceb4f73aeaa7cca26
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2015-08-05 18:11:26 -0700

    update for new field names

--- other/graph,big.hs
+++ other/graph,big.hs
@@ -13,11 +13,11 @@ let si = StmtIdx 1
     sn3 = StmtNode { _stmt = s3, _relIdxs = [ri2] } -- ri2: false (it's ri1)
     ri = RelIdx 1
     rni = RelNodeIdx ri
-    r = Rel { _template = si, _members = [sni2, sni3] } -- dog needs collar
+    r = Rel { _tplt = si, _mbrs = [sni2, sni3] } -- dog needs collar
     rn = RelNode  { _rel  = r, _relIdxs = [] }
     ri2 = RelIdx 2
     rni2 = RelNodeIdx ri2
-    r2 = Rel { _template = si, _members = [sni2, sni2] } -- dog needs dog
+    r2 = Rel { _tplt = si, _mbrs = [sni2, sni2] } -- dog needs dog
     rn2 = RelNode  { _rel  = r, _relIdxs = [] }
     g = Graph { _nodeMap = Map.insert sni sn $ Map.insert sni2 sn2
                 $ Map.insert sni3 sn3 $ Map.insert rni rn 
