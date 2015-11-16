ba69e87be34716393f729fbf4ccb0e2d01a17215
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2015-11-15 18:10:58 -0800

    Begin test suite.

--- src/Dwt.hs
+++ src/Dwt.hs
@@ -4,12 +4,29 @@ module Dwt
   , module Dwt -- exports everything in this file
     -- could be more selective
   -- , module Dwt.Graph -- etc. Will need to import below to match.
-  ) where                   
+  ) where
 import Data.Graph.Inductive
 
 myEmpty = empty :: Gr String String
-                           
+
 insString s g = insNode (i,s) g
   where i = head $ newNodes 1 g
 
+insStringEdge s n1 n2 g = insEdge (n1,n2,s) g
+
+-- 2015 11 13
+-- intention: use the Sum type below in the Graph type 
+  -- implementing eval as a recursive graph function
+
+data Sum = Sum {sum1 :: Sum, sum2 :: Sum} | SumConst Int deriving Show
+a = SumConst 3
+b = Sum a a
+
+eval :: Sum -> Int
+eval s = case s of
+  SumConst a -> a
+  Sum a b -> eval a + eval b
+  -- works! transcript:
+    -- *Dwt> (eval a, eval b)
+    -- (3,6)
 
