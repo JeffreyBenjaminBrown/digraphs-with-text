5dcc6daf56005eaca5c42c3632b24b399804c433
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2015-11-16 01:21:58 -0800

    reduce MmTrip (Int,Int,Int) -> MmTrip define alias Mindmap = Gr MmNode MmLab

--- src/Dwt.hs
+++ src/Dwt.hs
@@ -2,34 +2,40 @@
     module Dwt
   ( -- exports:
   module Data.Graph.Inductive -- export for testing, not production
+  , module List
   , module Dwt -- exports everything in this file
     -- could be more selective
   -- , module Dwt.Graph -- etc. Will need to import below to match.
   ) where    
     import Data.Graph.Inductive
-    import Data.String
+    import Data.String as String
+    import Data.List as List
 
 -- data
-    data MmNode = MmStr String | MmTrip (Node,Node,Node)
+    data MmNode = MmStr String.String | MmTrip
       deriving (Show,Eq,Ord)
       -- Adj b = (Node,Node,b) but Adj String = MmTrip would be confusg
 
     data MmLab = MmLab1 | MmLab2 | MmLab3 -- corresponding to MTrip's 3
       deriving (Show,Eq,Ord)
 
+    type Mindmap = Gr MmNode MmLab
+
 -- build mindmap
-    mmEmpty = empty :: Gr MmNode MmLab
+    mmEmpty = empty :: Mindmap
     
     insMmStr str g = insNode (int, MmStr str) g
       where int = head $ newNodes 1 g
     
-    insMmTrip (MmTrip (n,r,m)) g = insEdge (newNode,n,MmLab1)
-        $ insEdge (newNode,r,MmLab2)
-        $ insEdge (newNode,m,MmLab3)
-        $ insNode (newNode, MmTrip (n,r,m)) g -- add 1 node, 3 edges
+    insMmTrip (n,r,m) g = insEdge (newNode, n, MmLab1)
+                        $ insEdge (newNode, r, MmLab2)
+                        $ insEdge (newNode, m, MmLab3)
+                        $ insNode (newNode, MmTrip) g -- add 1 node, 3 edges
       where newNode = head $ newNodes 1 g
+
 -- query mindmap
-    -- mmMatch (Just n, Nothing, Nothing) =
+  --  mmMatch g (Just n, Nothing, Nothing) =
+    --  let outLEdges = out g n :: [LEdge MmLab]
       -- find all edges (m,n) labeled MmLab1
       -- return the list of those m values
     -- mmMatch (Nothing, Just n, Nothing)
