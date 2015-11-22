dfa2ce9e27e8ab6b9b4647841960d0d17767d048
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2015-11-21 21:31:13 -0800

    RelExpr -> Rel

--- src/Dwt.hs
+++ src/Dwt.hs
@@ -1,7 +1,7 @@
 -- usually folded
   -- TODO ? make "arity" a type
       -- its support is the integers in [1,k]
-      -- RelPositions and RelExprs contain one
+      -- RelPositions and Rels contain one
         -- or RelPosition contains a number that varies in [1,k]
           -- where k is the arity
   -- types, vocab, language
@@ -26,7 +26,7 @@
     import Control.Monad (mapM_)
 
 -- types
-    data MmExpr = MmString String | RelExpr Int
+    data MmExpr = MmString String | Rel Int
       -- TODO: add third type RelTplt String
       --       MmString -> MmString
       --       relExpr -> rel
@@ -48,7 +48,7 @@
       where len = length ns
             newNode = head $ newNodes 1 g
             g' = insEdge (newNode, t, RelTplt)
-               $ insNode (newNode, RelExpr len) g
+               $ insNode (newNode, Rel len) g
             f []     g = g
             f (p:ps) g = f ps $ insEdge (newNode, fst p, RelPos $ snd p) g
 
@@ -56,7 +56,7 @@
     mmReferents :: Mindmap -> MmEdge -> Int -> Node -> [Node]
     mmReferents g e arity n =
       let pdrNode (m,n,label) = m
-          isKAryRel m = lab g m == (Just $ RelExpr arity)
+          isKAryRel m = lab g m == (Just $ Rel arity)
       in [m | (m,n,label) <- inn g n, label == e, isKAryRel m]
 
     mmRelps :: Mindmap -> [Maybe Node] -> [Node] -- TODO: Exhaust patterns
@@ -75,7 +75,7 @@
     showExpr g n = case lab g n of
       Nothing -> Left $ "node " ++ (show n) ++ " not in graph"
       Just (MmString s) -> Right $ prefixNode s
-      Just (RelExpr _) -> Right $ prefixNode $ intercalate ", " 
+      Just (Rel _) -> Right $ prefixNode $ intercalate ", " 
            $ (\(a,b)->a++b) $ partitionEithers $ map f
            $ sortOn (\(_,_,l)->l) $ out g n
         where f (n,m,label) = showExpr g m
