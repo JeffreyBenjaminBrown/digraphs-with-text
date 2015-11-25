31393f6ef304845dcf76736f9e6adcfac1646f5a
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2015-11-25 13:00:57 -0800

    Refactoring complete! Tplt now keeps [String] not String.   removed duplicate code

--- src/Dwt.hs
+++ src/Dwt.hs
@@ -28,20 +28,13 @@
 -- types
   -- IN PROGRESS: refactoring Tplt to hold a list of Strings
     type Arity = Int
-    data MmExpr =   MmString String | Tplt Arity String    | Rel Arity
-      deriving (Show,Read,Eq,Ord)
     data MmExpr' = MmString' String | Tplt' Arity [String] | Rel' Arity
       deriving (Show,Read,Eq,Ord)
     data MmEdge = AsTplt | AsPos Arity -- MmEdgeLabel more accurate, but too long
       deriving (Show,Read,Eq,Ord)
-    type Mindmap =  Gr MmExpr  MmEdge
     type Mindmap' = Gr MmExpr' MmEdge
 
 -- build
-    insStr :: String -> Mindmap -> Mindmap
-    insStr str g = insNode (int, MmString str) g
-      where int = head $ newNodes 1 g
-
     insStr' :: String -> Mindmap' -> Mindmap' -- unchanged but for type
     insStr' str g = insNode (int, MmString' str) g
       where int = head $ newNodes 1 g
@@ -57,23 +50,6 @@
     insTplt' s g = insNode (newNode, stringToTplt s) g
       where newNode = head $ newNodes 1 g
 
-    insTplt :: String -> Mindmap -> Mindmap
-    insTplt s g = insNode (newNode, Tplt (countHoles s) s) g
-      where newNode = head $ newNodes 1 g
-            countHoles = length . filter (== '_') :: String -> Int
-
-    insRel :: Node -> [Node] -> Mindmap -> Mindmap -- TODO ? return Either Str Mm
-    insRel t ns g = if ti /= length ns -- t is tplt, otherwise like ns
-        then error "Tplt arity /= number of members"
-        else f (zip ns [1..ti]) g'
-      where Tplt ti ts = fromJust $ lab g t -- TODO: consider case of Nothing?
-                                            -- case of Just k, for k not Tplt?
-            newNode = head $ newNodes 1 g
-            g' = insEdge (newNode, t, AsTplt)
-               $ insNode (newNode, Rel ti) g
-            f []     g = g
-            f (p:ps) g = f ps $ insEdge (newNode, fst p, AsPos $ snd p) g
-
     insRel' :: Node -> [Node] -> Mindmap' -> Mindmap' -- TODO ? return Either
       -- this function is unchanged from insRel except in its type sig
     insRel' t ns g = if ti /= length ns -- t is tplt, otherwise like ns
@@ -87,15 +63,10 @@
             f []     g = g
             f (p:ps) g = f ps $ insEdge (newNode, fst p, AsPos $ snd p) g
 
--- query. TODO: Duplicate for MmExpr'
+-- query
     users' :: Mindmap' -> MmEdge -> Arity -> Node -> [Node] -- unchanged
     users' g e k n = -- returns all uses (of a type specified by e & k) of n
       let isKAryRel m = lab g m == (Just $ Rel' k)
-      in [m | (m,n,label) <- inn g n, label == e, isKAryRel m]
-
-    users :: Mindmap -> MmEdge -> Arity -> Node -> [Node]
-    users g e k n = -- returns all uses (of a type specified by e & k) of n
-      let isKAryRel m = lab g m == (Just $ Rel k)
       in [m | (m,n,label) <- inn g n, label == e, isKAryRel m]
 
     mmRelps' :: Mindmap' -> [Maybe Node] -> [Node] -- unchanged
@@ -107,25 +78,11 @@
             listIntersect [] = [] -- silly case
             listIntersect (x:xs) = foldl intersect x xs
 
-    mmRelps :: Mindmap -> [Maybe Node] -> [Node]
-    mmRelps g mns = listIntersect $ map f jns
-      where arity = length mns - 1
-            jns = filter (isJust . fst) $ zip mns [0..] :: [(Maybe Node, Int)]
-            f (Just n, 0) = users g AsTplt    arity n
-            f (Just n, k) = users g (AsPos k) arity n
-            listIntersect [] = [] -- silly case
-            listIntersect (x:xs) = foldl intersect x xs
-
 -- view
     subInTplt' :: MmExpr' -> [String] -> String
     subInTplt' (Tplt' k ts) ss = let pairList = zip ts $ ss ++ [""] --append [""] because there are n+1 segments in an n-ary Tplt
       in foldl (\s (a,b) -> s++a++b) "" pairList
     subInTplt' _ _ = error "MmExpr not a Tplt"
-
-    subInTplt :: String -> [String] -> String -- TODO: Tplt be already split
-    subInTplt t ss = let tpltAsList = splitTpltStr t
-                         pairList = zip tpltAsList $ ss ++ [""] --append [""] because there are n+1 segments in an n-ary Tplt
-      in foldl (\s (a,b) -> s++a++b) "" pairList
 
     showExpr' :: Mindmap' -> Node -> String -- changd: no either. TODO: BUGGY
        -- replacing showExpr; this one is Tplt'-aware
@@ -149,29 +106,6 @@
         in subInTplt' t $ map (\(_,m,_) -> showExpr' g m) ledges
       where prefixNode s = (show n) ++ ": " ++ s
 
-    showExpr :: Mindmap -> Node -> Either String String -- TODO ? cyc-robust
-      -- if the graph is recursive, this could infinite loop
-        -- although that kind of mindmap is unlikely, difficult
-          -- c.f. Godel's impossibility theorem
-      -- a solution: while building, keep list of visited nodes
-        -- if visiting one already there, display it as just its number
-        -- or number + "already displayed higher in this (node view?)"
-    showExpr g n = case lab g n of
-      Nothing -> Left $ "node " ++ (show n) ++ " not in graph"
-      Just (MmString s) -> Right $ prefixNode s
-      Just (Tplt k s) -> Right $ prefixNode $ "Tplt: " ++  s
-      Just (Rel _) -> Right $ prefixNode $ intercalate ", " 
-           $ (\(a,b) -> a++b) $ partitionEithers $ map f
-           $ sortOn (\(_,_,l)->l) $ out g n
-        where f (n,m,label) = showExpr g m
-      where prefixNode s = (show n) ++ ": " ++ s
-
-    view :: Mindmap -> [Maybe Node] -> IO ()
-    view g mns = mapM_ putStrLn 
-               $ map (eitherString . showExpr g) $ mmRelps g mns
-      where eitherString (Left s) = s
-            eitherString (Right s) = s
-
     view' :: Mindmap' -> [Maybe Node] -> IO () -- changed; no eitherString
     view' g mns = mapM_ putStrLn 
-               $ map (showExpr' g) $ mmRelps' g mns
+                $ map (showExpr' g) $ mmRelps' g mns
--- test/Spec.hs
+++ test/Spec.hs
@@ -7,47 +7,14 @@
     main = runTestTT testList
 
     testList = TestList
-      [   TestLabel "tSubInTplt" tSubInTplt
---        , TestLabel "tSubInTplt'" tSubInTplt'
-        , TestLabel "tInsert" tInsert
+      [   TestLabel "tSubInTplt'" tSubInTplt'
         , TestLabel "tInsert'" tInsert'
-        , TestLabel "tRelvs"   tRelvs
-        , TestLabel "tRelvs'"   tRelvs
+        , TestLabel "tRelvs'"  tRelvs'
       ]
 
 -- "globals"
-  -- orig
-    g1,g1' :: Mindmap
+    g1,g1' :: Mindmap'
     g1 = mkGraph
-      [   (0, MmString "dog"       )
-        , (1, Tplt 2 "_ wants _" )
-        , (2, Tplt 2 "_ needs _" )
-        , (3, MmString "water"     )
-        , (4, MmString "brandy"    )
-        , (5, Rel 2           )
-        , (6, Rel 2           )
-        , (7, Tplt 3 "_ needs _ for _")
-        , (8, Rel 3           ) 
-        , (9, Tplt 2 "statement _ is _")
-        , (10, MmString "dubious"  )
-        , (11, Rel 2          )
-      ] [ (5,1, AsTplt), (5,0, AsPos 1), (5,4,AsPos 2)
-        , (6,2, AsTplt), (6,0, AsPos 1), (6,3,AsPos 2)
-        , (8,7, AsTplt), (8,0, AsPos 1), (8,3,AsPos 2), (8,4,AsPos 3) 
-        , (11,9,AsTplt), (11,5,AsPos 1), (11,10,AsPos 2)
-      ]
-
-    g1' =   insRel 9 [5,10] 
-          $ insStr "dubious"    $ insTplt "statement _ is _"
-          $ insRel 7 [0,3,4]    $ insTplt "_ needs _ for _"
-          $ insRel 2 [0,3]      $ insRel 1 [0,4]
-          $ insStr "brandy"     $ insStr "water"
-          $ insTplt "_ needs _" $ insTplt "_ wants _"
-          $ insStr "dog"        $ empty :: Mindmap
-
-  -- next gen
-    g2,g2' :: Mindmap'
-    g2 = mkGraph
       [   (0, MmString' "dog"       )
         , (1, stringToTplt "_ wants _" )
         , (2, stringToTplt "_ needs _" )
@@ -66,7 +33,7 @@
         , (11,9,AsTplt), (11,5,AsPos 1), (11,10,AsPos 2)
       ]
 
-    g2' =   insRel' 9 [5,10] 
+    g1' =   insRel' 9 [5,10] 
           $ insStr' "dubious"     $ insTplt' "statement _ is _"
           $ insRel' 7 [0,3,4]    $ insTplt' "_ needs _ for _"
           $ insRel' 2 [0,3]      $ insRel' 1 [0,4]
@@ -75,33 +42,15 @@
           $ insStr' "dog"        $ empty :: Mindmap'
 
 -- tests
-    tSubInTplt = let t1 = "_ needs _"
-                     t2 = "Does _ need _?"
-                     t3 = "_ needs _!"
-                     (a,b) = ("skeletor","love")
-      in TestCase $ do
-        assertBool "1" $ subInTplt t1 [a,b] == "skeletor needs love"
-        assertBool "2" $ subInTplt t2 [a,b] == "Does skeletor need love?"
-        assertBool "3" $ subInTplt t3 [a,b] == "skeletor needs love!"
-
     tSubInTplt' = TestCase $ do
-      assertBool "1" $ subInTplt' (fromJust $ lab g2 1) ["man","peace"]
+      assertBool "1" $ subInTplt' (fromJust $ lab g1 1) ["man","peace"]
         == "man wants peace"
 
-    tInsert = TestCase $ do
+    tInsert' = TestCase $ do
       assertBool "insRel & insStr" $ g1 == g1'
 
-    tInsert' = TestCase $ do
-      assertBool "insRel & insStr" $ g2 == g2'
-
-    tRelvs = TestCase $ do
-      assertBool "1--"  $ mmRelps g1 [Just 1, Nothing, Nothing] == [5]
-      assertBool "-0-"  $ mmRelps g1 [Nothing, Just 0, Nothing] == [5,6]
-      assertBool "--3"  $ mmRelps g1 [Nothing, Nothing, Just 4] == [5]
-      assertBool "---4" $ mmRelps g1 [Nothing, Nothing, Nothing, Just 4] == [8]
-
     tRelvs' = TestCase $ do
-      assertBool "1--"  $ mmRelps' g2 [Just 1,  Nothing, Nothing] == [5]
-      assertBool "-0-"  $ mmRelps' g2 [Nothing, Just 0,  Nothing] == [5,6]
-      assertBool "--3"  $ mmRelps' g2 [Nothing, Nothing, Just 4 ] == [5]
-      assertBool "---4" $ mmRelps' g2 [Nothing, Nothing, Nothing, Just 4] == [8]
+      assertBool "1--"  $ mmRelps' g1 [Just 1,  Nothing, Nothing] == [5]
+      assertBool "-0-"  $ mmRelps' g1 [Nothing, Just 0,  Nothing] == [5,6]
+      assertBool "--3"  $ mmRelps' g1 [Nothing, Nothing, Just 4 ] == [5]
+      assertBool "---4" $ mmRelps' g1 [Nothing, Nothing, Nothing, Just 4] == [8]
