940dd706ebb1eed992b69d35ba1843f763a3ef71
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2015-11-30 14:30:26 -0800

    simplify monad_fail_problems.hs

--- test/monad_fail_problems.hs
+++ test/monad_fail_problems.hs
@@ -1,28 +1,31 @@
-    import Data.Graph.Inductive
+    import Data.Graph.Inductive -- FGL, the Functional Graph Library
 
     type MyGraph = Gr String String
 
-    tinyGraph = mkGraph
-      [   (0, "dog")
-        , (2, "yams")
-      ] [] :: MyGraph
+    tinyGraph = mkGraph [(0, "dog")] [] :: MyGraph
+
+    maybeSucceed = replaceStringAtNodeM tinyGraph 0 "cat" :: Maybe MyGraph
+      -- == Just (mkGraph [(0,"cat")] [])
+    maybeFail = replaceStringAtNodeM tinyGraph 1 "cat" :: Maybe MyGraph
+      -- == Nothing
+
+    eitherSucceed = replaceStringAtNodeM tinyGraph 0 "cat" :: Either String MyGraph
+      -- ==  Right (mkGraph [(0,"cat")] [])
+    eitherFail = replaceStringAtNodeM tinyGraph 1 "cat" :: Either String MyGraph
+      -- *** Exception: Node not in Graph
 
     gelemM :: (Monad m) => MyGraph -> Node -> m ()
-    gelemM g n = if gelem n g then return () 
-                              else fail "Node not in Graph"
+    gelemM g n = if gelem n g       -- FGL's gelem function returns
+      then return ()                  -- True if the node is in the graph
+      else fail "Node not in Graph"   -- False otherwise
 
-    chStringAt :: MyGraph -> Node -> String -> MyGraph
-    chStringAt g n e = let (Just (a,b,c,d),g') = match n g
+    replaceStringAtNode :: MyGraph -> Node -> String -> MyGraph
+    replaceStringAtNode g n e = let (Just (a,b,c,d),g') = match n g
       in (a,b,e,d) & g'
 
-    chStringAt' :: (Monad m) => MyGraph -> Node -> String -> m MyGraph
-    chStringAt' g n s = do
+    replaceStringAtNodeM :: (Monad m) => MyGraph -> Node -> String -> m MyGraph
+    replaceStringAtNodeM g n s = do
       gelemM g n
-      return $ chStringAt g n s
-
-    maybeSucceed = chStringAt' tinyGraph 0 "cat" :: Maybe MyGraph
-    maybeFail = chStringAt' tinyGraph 1 "cat" :: Maybe MyGraph
-
-    eitherSucceed = chStringAt' tinyGraph 0 "cat" :: Either String MyGraph
-    eitherFail = chStringAt' tinyGraph 1 "cat" :: Either String MyGraph
-      -- why is eitherFail an Exception, when maybeFail is a Nothing?
+      return $ replaceStringAtNode g n s
+        -- if evaluated, the pattern match in replaceStringAtNode (Just ...) must succeed,
+        -- because gelemM catches the case where n is not in the graph
