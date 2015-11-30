ec2cb8cb59631606627f17555483f5d062f7dc2a
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2015-11-29 22:45:23 -0800

    add a minimal graph

---
+++ data/minimalGraph.hs
@@ -0,0 +1,11 @@
+import Dwt
+
+minimalGraph = -- This graph contains the relationship "[the dog] needs [water]" and its constituent elements.
+  mkGraph
+      [   (0, Str "the dog")
+        , (1, stringToTplt "_ wants _")
+        , (2, Str "water")
+        , (3, Rel 2)
+      ] 
+      [(3,1, AsTplt), (5,0, AsPos 1), (5,2,AsPos 2)]
+      :: Mindmap
