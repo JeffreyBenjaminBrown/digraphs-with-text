69119676034a373b5ac57644824e7d52299e3d6f
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2015-12-02 13:37:24 -0800

    remove cruft

--- test/proof, obsolete.hs
+++
@@ -1,5 +0,0 @@
--- proving that multi-edges work (try evaluating "lsuc (test) 1")
-test = 
-  insStringEdge "another conn" 1 0
-  $ insStringEdge "one conn" 1 0
-  $ insString "second" $ insString "first" $ myEmpty
