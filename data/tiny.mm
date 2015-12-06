50783749629a1f2554336ab7b106fbd48936a89f
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2015-12-06 00:35:53 -0800

    add fonts to test .mm dataset

--- data/tiny.mm
+++ data/tiny.mm
@@ -1,55 +1,62 @@
 <map version="freeplane 1.3.0">
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
-<node TEXT="the root" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1449261674074"><hook NAME="MapStyle" zoom="2.357">
+<node TEXT="the root" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1449261674074">
+<hook NAME="AutomaticEdgeColor" COUNTER="5"/>
+<hook NAME="MapStyle" background="#d9e3e4" zoom="1.241">
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node">
 <stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
-<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
-<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="800" COLOR="#000000" STYLE="as_parent">
+<font NAME="Courier" SIZE="16" BOLD="false" ITALIC="false"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
-<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
-<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<stylenode LOCALIZED_TEXT="defaultstyle.details" MAX_WIDTH="800" COLOR="#cc00ff">
+<font NAME="Courier" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" MAX_WIDTH="800">
+<font NAME="Courier" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" MAX_WIDTH="800">
 <edge STYLE="hide_edge"/>
 <cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<font NAME="Courier" SIZE="16"/>
 </stylenode>
 </stylenode>
 <stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
-<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
-<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#009999" STYLE="fork" MAX_WIDTH="800">
+<font NAME="Courier" SIZE="16" BOLD="true"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
-<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#ff7700" BACKGROUND_COLOR="#c6f7f9" STYLE="fork" MAX_WIDTH="800">
+<font NAME="Courier" SIZE="16" BOLD="true"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
-<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#008600" BACKGROUND_COLOR="#f5e1db" MAX_WIDTH="800">
+<font NAME="Courier" SIZE="16" BOLD="true"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="styles.important">
+<stylenode LOCALIZED_TEXT="styles.important" MAX_WIDTH="800">
 <icon BUILTIN="yes"/>
+<font NAME="Courier" SIZE="16"/>
 </stylenode>
 </stylenode>
 <stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
-<font SIZE="18"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000099" MAX_WIDTH="800">
+<font NAME="Courier" SIZE="16" BOLD="true"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
-<font SIZE="16"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#006600" MAX_WIDTH="800">
+<font NAME="Courier" SIZE="16" BOLD="true"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
-<font SIZE="14"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#996600" MAX_WIDTH="800">
+<font NAME="Courier" SIZE="16" BOLD="true"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
-<font SIZE="12"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000" MAX_WIDTH="800">
+<font NAME="Courier" SIZE="16" BOLD="true"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
-<font SIZE="10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#660066" MAX_WIDTH="800">
+<font NAME="Courier" SIZE="16" BOLD="true"/>
 </stylenode>
 </stylenode>
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="4"/>
 <node TEXT="1, linked to 2 and 2.1" POSITION="right" ID="ID_1588678987" CREATED="1449192253308" MODIFIED="1449261657470">
 <edge COLOR="#ff0000"/>
 <arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="80" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1481921831" STARTINCLINATION="40;0;" ENDINCLINATION="40;0;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
@@ -64,7 +71,25 @@
 <edge COLOR="#0000ff"/>
 <node TEXT="2.1" ID="ID_1441026734" CREATED="1449192255923" MODIFIED="1449261628551"/>
 </node>
-<node TEXT="fat node, with a newline right here:&#xa;these are the escape characters: &lt;&gt;&quot;&amp;&apos;&#xa;caret is not: ^" POSITION="right" ID="ID_1716485631" CREATED="1449193573345" MODIFIED="1449216947328">
+<node TEXT="fonts" LOCALIZED_STYLE_REF="default" POSITION="right" ID="ID_396524263" CREATED="1449389377147" MODIFIED="1449389475617">
+<edge COLOR="#00ffff"/>
+<node TEXT="bold" ID="ID_183490876" CREATED="1449389379689" MODIFIED="1449389381818">
+<font BOLD="true"/>
+</node>
+<node TEXT="italic" ID="ID_1025225234" CREATED="1449389382353" MODIFIED="1449389385296">
+<font ITALIC="true"/>
+</node>
+<node TEXT="c1, blue" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" ID="ID_1693927148" CREATED="1449389386896" MODIFIED="1449389510569"/>
+<node TEXT="c2, green" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" ID="ID_849605525" CREATED="1449389479383" MODIFIED="1449389511375"/>
+<node TEXT="c3, gold" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1033943189" CREATED="1449389483215" MODIFIED="1449389512135"/>
+<node TEXT="c4, red" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1413545656" CREATED="1449389485935" MODIFIED="1449389512840"/>
+<node TEXT="c5, pink" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_1729964627" CREATED="1449389491200" MODIFIED="1449389513593"/>
+<node TEXT="cs1, ice" LOCALIZED_STYLE_REF="styles.topic" ID="ID_1315400225" CREATED="1449389494271" MODIFIED="1449389519928"/>
+<node TEXT="cs2, green on purple|peach" LOCALIZED_STYLE_REF="styles.subsubtopic" ID="ID_1146442192" CREATED="1449389499343" MODIFIED="1449389585540"/>
+<node TEXT="cs3, gold on electric blue" LOCALIZED_STYLE_REF="styles.subtopic" ID="ID_281756599" CREATED="1449389500295" MODIFIED="1449389593866"/>
+<node TEXT="cs4, purple" LOCALIZED_STYLE_REF="AutomaticLayout.level,4" ID="ID_668159818" CREATED="1449389501087" MODIFIED="1449389602703"/>
+</node>
+<node TEXT="fat node, with a newline right here:&#xa;these are the escape characters: &lt;&gt;&quot;&amp;&apos;&#xa;caret is not: ^" LOCALIZED_STYLE_REF="default" POSITION="right" ID="ID_1716485631" CREATED="1449193573345" MODIFIED="1449389442603">
 <edge COLOR="#ff00ff"/>
 </node>
 </node>
