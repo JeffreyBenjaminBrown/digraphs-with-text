fb34b08944c2b4eb97c9bb6a44ade6a2466d258c
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2016-01-16 22:58:47 -0800

    comments

--- src/Dwt/FileIO.hs
+++ src/Dwt/FileIO.hs
@@ -10,12 +10,15 @@
     -- each line corresponds either to a node, or to the members of a Rel or Coll
       -- (and always in the same order)
     -- so that if you edit a graph, the diff is easily human-readable
+    -- TRICKY: uses the fact that labEdges and labNodes sort their output
     -- TODO ? could be more general (in 2 ways)
       -- currently it groups on whether the first coordinates are equal
         -- it assumes that elts with the same first coord are contiguous
         -- in a more general func, that could be forced by sorting first (1 way)
       -- "take the first coord" could be a function argument (the 2nd way)
-    _groupEdges :: [LEdge b] -> [[LEdge b]] -> [[LEdge b]]
+    _groupEdges :: [LEdge b] -> -- what's left of the input
+                   [[LEdge b]] -> -- accumulator, recursive
+                   [[LEdge b]]
     _groupEdges [] els = els
     _groupEdges (e:es) [] = _groupEdges es [[e]]
     _groupEdges   (( a, b, c):es )  
