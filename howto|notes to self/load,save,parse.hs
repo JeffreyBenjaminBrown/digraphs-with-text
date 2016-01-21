f40c6e1b3ee2b79d1d0374d93545facc1ce64ae6
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2016-01-20 20:31:22 -0800

    Done testing! It works!

--- howto|notes to self/load,save,parse.hs
+++ howto|notes to self/load,save,parse.hs
@@ -1,20 +1,23 @@
 -- load
   gf <- readFile "data/agent.dwt"
   let g = read gf :: Mindmap
+  let g' = read gf :: Mindmap'
 
 -- save
   writeFile "data/agent.dwt" $ graphToText g
 
 -- parse
-  -- run "stack ghci" and load the test suite
-    :l test/TGraph.hs 
-
   -- start with a .mm file with no html text
-    mls <- mmToMlTags "untracked/data/agent.mm"
+    mls <- mmToMlTags "data/agent.mm"
     let mls2 = collapseRich $ stripRichTags $ fromRight mls
-
-  -- this slightly-longer-than-necessary process turns it into a Mindmap called g
     let spec = fromRight $ dwtSpec mls2
+
+  -- with redundant Arities
     let fr = frame $ frameOrphanStyles spec :: Either String DwtFrame
     let fWithNodes = fromRight $ loadNodes (spec, fromRight fr)
-    let g = compressGraph $ fromRight $ loadEdges spec fWithNodes
+    let g = compressGraph $ fromRight $ loadEdges spec fWithNodes :: Mindmap
+
+  -- with redundant Arities
+    let fr' = frame' $ frameOrphanStyles' spec :: Either String DwtFrame'
+    let fWithNodes' = fromRight $ loadNodes' (spec, fromRight fr')
+    let g' = compressGraph $ fromRight $ loadEdges' spec fWithNodes' :: Mindmap'
