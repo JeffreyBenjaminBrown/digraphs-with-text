e225e7430648b83fe516ff562199f3f2ff353056
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2016-01-22 13:02:57 -0800

    Add plans to refactor types!

---
+++ src/Dwt/TODO_Types.hs
@@ -0,0 +1,16 @@
+    type Mindmap = Gr Expr DwtEdge
+    data Expr = Str String | Tplt [String] | Rel | Coll String
+              | RelSpecExpr RelVarSpec
+    data DwtEdge = RoleEdge Role | CollMbr
+    data Role = TpltRole | MbrRole RelPos
+      deriving (Show,Read,Eq,Ord)
+
+    data MbrVar = It | Any | Ana | Kata -- TODO: can oft (always?) omit the Any
+    data MbrSpec = VarSpec MbrVar | MbrSpec Node
+
+    type RelVarSpec = Map.Map RelRole MbrVar
+      -- specifies a subset of what a RelSpec does
+      -- the other information is carried external to it, in the graph
+    type RelSpec = Map.Map RelRole MbrSpec
+      -- if well-formed, has a Tplt, and has RelPoss from 1 to the Tplt's Arity
+      -- (but anything mapping to Any can be dropped)
