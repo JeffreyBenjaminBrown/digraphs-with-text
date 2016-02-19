02d47c8e4ff6ab7ef52ff213a08373dc36976dc2
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2016-02-18 22:53:37 -0800

    more

--- src/Dwt/Search.hs
+++ src/Dwt/Search.hs
@@ -8,13 +8,34 @@
     import Dwt.Graph
     import Data.Graph.Inductive
 
+    import Data.Map as Map
+
     data QNode = QNode Node | QStr String | QTplt [String] | QRel QNode [QNode]
 
-    qGet :: QNode -> Mindmap -> Either String Node
-    qGet (QNode n) _ = Right n
-    qGet (QStr s) g = do
-      let ns = nodes $ labfilter (\n -> case n of Str t -> s==t; _ -> False)
-               $ gmap (\(_,b,c,_) -> ([], b, c, [])) g
+    lengthOne :: [a] -> Either String ()
+    lengthOne ns = do
       if length ns == 0 then Left "absent" else return ()
       if length ns > 1 then Left "multiple" else return ()
+
+    edgeless :: Mindmap -> Mindmap
+    edgeless = gmap (\(_,b,c,_) -> ([], b, c, []))
+
+    qGet :: QNode -> Mindmap -> Either String Node
+    qGet (QNode n) g = gelemM g n >> Right n
+    qGet (QStr s) g = do
+      let ns = nodes $ labfilter (\n -> case n of Str t -> s==t; _ -> False)
+               $ edgeless g
+      lengthOne ns
       Right $ head ns
+    qGet (QTplt s) g = do
+      let ns = nodes $ labfilter (\n -> case n of Tplt t -> s==t; _ -> False)
+               $ edgeless g
+      lengthOne ns
+      Right $ head ns
+--    qGet (QRel qt qes) g = do
+--      let tn = qGet qt g
+--          es = mapM (flip qGet g) es
+--          rs = Map.fromList $ tn : es :: RelSpec
+--          ns = matchRel g rs
+--      lengthOne ns -- ifdo clarify: errors look the same as those from qGet above
+--      Right $ head ns
