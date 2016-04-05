19db5f2700e32bbed6b9a79b56b3a5248ae90cf0
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2016-04-05 13:32:58 -0700

    Regex search!

--- src/Dwt/Search.hs
+++ src/Dwt/Search.hs
@@ -5,10 +5,13 @@
       module Dwt.Search
     ) where
 
+    import Text.Regex
+
     import Dwt.Graph
     import Data.Graph.Inductive
 
     import Data.Map as Map
+    import Data.Maybe as Mb
 
     -- queries
     data QNode = QNode Node -- when you already know the Node
@@ -21,7 +24,7 @@
       if length ns == 0 then Left "absent" else return ()
       if length ns > 1 then Left "multiple" else return ()
 
-    edgeless :: Gr a b -> Gr a b
+    edgeless :: Gr a b -> Gr a b -- ? faster or slower
     edgeless = gmap (\(_,b,c,_) -> ([], b, c, []))
 
     qGet :: Mindmap -> QNode -> Either String Node
@@ -37,16 +40,26 @@
       lengthOne ns
       Right $ head ns
 
+    qRegexStr :: Mindmap -> String -> Either String [Node]
+    qRegexStr g s = do
+      let r = mkRegex s
+      let ns = nodes $ labfilter 
+                       (\lab -> case lab of Str t -> Mb.isJust $ matchRegex r t;
+                                            _ -> False)
+                       $ edgeless g
+      Right ns
+
     qInsRel :: QNode -> [QNode] -> Mindmap -> Either String Mindmap
     qInsRel qtn qns g = do
       tn <- qGet g qtn 
       ns <- mapM (qGet g) qns
       insRel tn ns g
 
---    qGet g (QRel qt qes) = do
---      let tn = qGet qt g
---          es = mapM (flip qGet g) es
---          rs = Map.fromList $ tn : es :: RelSpec
---          ns = matchRel g rs
---      lengthOne ns -- ifdo clarify: errors look the same as those from qGet above
---      Right $ head ns
+-- very stale
+    --    qGet g (QRel qt qes) = do
+    --      let tn = qGet qt g
+    --          es = mapM (flip qGet g) es
+    --          rs = Map.fromList $ tn : es :: RelSpec
+    --          ns = matchRel g rs
+    --      lengthOne ns -- ifdo clarify: errors look the same as those from qGet above
+    --      Right $ head ns
