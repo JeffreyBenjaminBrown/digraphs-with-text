10caec6563ecfd77ce1a4153923469cc90ac4aae
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2016-04-21 19:45:01 -0700

    data ViewProg = ViewProg {vpPrefixer :: Prefixer, vpShowFiats :: ShowFiats}   create and use; maybe should have been two commits

--- src/Dwt/View.hs
+++ src/Dwt/View.hs
@@ -18,12 +18,19 @@
     type Depth = Int
     type Gen = (Depth,[Node])
 
+    type ShowFiats = Map.Map Node String -- lookup how to show some special Nodes
+      -- todo ! use for shorthand like It
+
     data Prefixer = Prefixer {
         _str :: (Node -> String) -- Str|Fl -> String
       , _tplt :: (Node -> String)
       , _rel :: (Node -> Node -> String) -- Rel -> Tplt -> String
       , _coll :: (Node -> String)
       }
+
+    data ViewProg = ViewProg { vpPrefixer :: Prefixer
+                             , vpShowFiats :: ShowFiats
+                             }
 
 -- things _showExpr uses, maybe useful elsewhere -- TODO ? export|promote x-file
     exprDepth :: Mindmap -> Node -> Depth -- TODO ? Use the [Node]
@@ -44,22 +51,21 @@
 
 -- _showExpr and things only it uses
     _showExpr :: Depth ->
-                 Map.Map Node String -> -- todo ! use for shorthand like It
-                 Prefixer -> 
+                 ViewProg ->
                  Mindmap -> Maybe Node -> String
-    _showExpr d subs pfxr g Nothing = "#absent_node#"
-    _showExpr d subs pfxr g (Just n) = 
-      let show_maybe_node mn = _showExpr (d+1) subs pfxr g mn
-      in case Map.lookup n subs of
+    _showExpr d vp g Nothing = "#absent_node#"
+    _showExpr d vp g (Just n) = 
+      let show_maybe_node mn = _showExpr (d+1) vp g mn
+      in case Map.lookup n (vpShowFiats vp) of
 
         Just s -> s
         Nothing -> case lab g n of
           Nothing -> error $ "showExpr: node " ++ (show n) ++ " not in graph"
-          Just (Str s)   -> (_str pfxr) n ++ s
-          Just (Fl f)   -> (_str pfxr) n ++ show f
-          Just t@(Tplt ts) -> (_tplt pfxr) n ++
+          Just (Str s)   -> (_str $ vpPrefixer vp) n ++ s
+          Just (Fl f)   -> (_str $ vpPrefixer vp) n ++ show f
+          Just t@(Tplt ts) -> (_tplt $ vpPrefixer vp) n ++
             (subInTplt t $ replicate (tpltArity t) "_")
-          Just (Coll)    -> (_coll pfxr) n ++ "TODO: use name" ++ ": "
+          Just (Coll)    -> (_coll $ vpPrefixer vp) n ++ "TODO: use name" ++ ": "
             ++ ( intercalate ", "
                $ map (show_maybe_node . Just) 
                      [m | (m,CollEdge CollMbr) <- lsuc g n] )
@@ -73,18 +79,16 @@
                 showMbConcreteMbr ms = case ms of
                   VarSpec var -> show var
                   NodeSpec node -> show_maybe_node $ Just node
-            in ((_rel pfxr) n tpltNode ++)  $  ("#RelSpec#: " ++)
+            in ((_rel $ vpPrefixer vp) n tpltNode ++)  $  ("#RelSpec#: " ++)
               $ subInTplt tpltLab 
               $ map showMbConcreteMbr $ map snd rsl
 
-          Just (Rel) -> _showRel Rel d subs pfxr g n
+          Just (Rel) -> _showRel d vp g n
 
-    _showRel :: Expr ->
-                Depth ->
-                Map.Map Node String ->
-                Prefixer -> 
+    _showRel :: Depth ->
+                ViewProg ->
                 Mindmap -> Node -> String
-    _showRel Rel d subs pfxr g n =
+    _showRel d vp g n =
       let elts = Map.fromList $ map (\(adr,elab)->(elab,Just adr))
                               $ lsuc g n :: Map.Map DwtEdge (Maybe Node)
             -- in a well-formed graph, any edge label emits
@@ -97,9 +101,9 @@
             (nullMbrMap tpltExpr) :: [Maybe Node]
             -- handles missing nodes
             -- todo ? ordered list bad; just pass map
-      in ((_rel pfxr) n tpltAddr ++)
+      in ((_rel $ vpPrefixer vp) n tpltAddr ++)
          $ subInTpltWithDollars tpltExpr
-           (map (_showExpr (d-1) subs pfxr g) memberNodes)
+           (map (_showExpr (d-1) vp g) memberNodes)
            d
 
     nullMbrMap :: Expr -> Map.Map DwtEdge (Maybe Node) 
@@ -122,12 +126,16 @@
       where f = const ""
 
     showExpr :: Mindmap -> Node -> String
-    showExpr g n = _showExpr d Map.empty prefixerDefault g (Just n)
+    showExpr g n = _showExpr d vp g (Just n)
       where d = exprDepth g n
+            vp = ViewProg { vpPrefixer = prefixerDefault
+                          , vpShowFiats = Map.empty }
 
     showExprT :: Mindmap -> Node -> String -- terse, no addresses
-    showExprT g n = _showExpr d Map.empty prefixerTerse g (Just n)
+    showExprT g n = _showExpr d vp g (Just n)
       where d = exprDepth g n
+            vp = ViewProg { vpPrefixer = prefixerDefault
+                          , vpShowFiats = Map.empty }
 
     -- TODO NEXT: count clarifications
 
