015563053545da1b056819f14e6afd70f299fd6c
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2016-06-15 20:08:58 -0700

    comments

--- src/Dwt/Minimal_Types.hs
+++ src/Dwt/Minimal_Types.hs
@@ -1,23 +1,21 @@
 -- This file is for illustration purposes only; it is not part of the codebase.
 
-    import Data.Graph.Inductive
+import Data.Graph.Inductive
 
-    type Arity = Int -- tuples have Arity: pairs have arity 2, triples arity 3 ..
-    type RelPos = Int -- the k members of a k-ary Rel take RelPos values [1..k]
+type Arity = Int -- tuples have Arity: pairs have arity 2, triples arity 3 ..
+type RelPos = Int -- the k members of a k-ary Rel take RelPos values [1..k]
 
-    type RSLT = Gr Expr RSLTEdge -- Recursive Set of Labeled Tuples
-      -- Read this as "a graph (Gr) in which
-      -- each node is an Expr and each edge is an RSLTEdge"
+type RSLT = Gr Expr RSLTEdge -- Recursive Set of Labeled Tuples
+  -- Read this as "a graph (Gr) in which
+  -- each node is an Expr and each edge is an RSLTEdge"
 
-    data Expr = Str String
-              | Rel  -- labeled tuple (LT), like "[cats] need [sun]"
-              | Tplt [String] -- template for a labeled tuple, like "_ did _ to _"
+data Expr = Str String
+          | Rel  -- labeled tuple (LT), like "[cats] need [sun]"
+          | Tplt [String] -- template for a labeled tuple, like "_ did _ to _"
 
-    data RSLTEdge = RelTplt | Mbr RelPos
-      -- Rule 1: any Expr can receive an edge (be a member of a superexpression),
-        -- but only Rels emit edges (have subexpressions).
-      -- Rule 2: Each k-ary Rel emits exactly one RelTplt edge and k Mbr edges
-        -- with the Mbr edges labeled 1 through k
-
-    -- The function definitions respect the above two rules, but the rules
-    -- are not themselves reified. I suspect they could be.
+data RSLTEdge = RelTplt | Mbr RelPos -- !Not every possible edge is valid!
+  -- Rule 1: any Expr can receive an edge (be a member of a superexpression),
+    -- but only Rels emit edges (have subexpressions).
+  -- Rule 2: Each k-ary Rel emits exactly one RelTplt edge and k Mbr edges
+    -- with the Mbr edges labeled 1 through k
+  --These rules are not reified; respecting them is the coder's responsibility.
