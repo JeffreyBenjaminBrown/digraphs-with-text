ae23407a978aecdd33e6770da099d624e77b6e0f
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2016-09-24 00:16:27 -0700

    tryIO: ask the user something first InsWord: implement

--- src/Dwt/Wiz.hs
+++ src/Dwt/Wiz.hs
@@ -6,46 +6,40 @@
     import Dwt.Graph
     import Dwt.Show
 
-    data Cmd = Id | InsTplt | Quit deriving (Show, Read) --Eq
+    data Cmd = Id | InsWord | InsTplt | InsRel | Quit deriving (Show, Read) --Eq
     type State = RSLT
 
     wiz :: State -> IO State
     wiz g = do
       putStrLn "Command me!"
       cmd <- (\s->read s::Cmd) <$> getLine
-      case cmd of Id -> wiz g
-                  Quit -> return g
-                  InsTplt -> tryIO ((>0) . length . filter (=='_'))
-                                   insTplt g
+      case cmd of 
+        Id -> wiz g
+        Quit -> return g
+        InsTplt -> tryIO "Enter a Tplt to add"
+                         ((>0) . length . filter (=='_'))
+                         insTplt g
+        InsWord ->  tryIO "Enter a Str to add"
+                          (const True)
+                          insWord g
+        -- InsRel -> insWordWiz g
 
-    tryIO :: (String-> Bool)
+    -- specifically State \vart IO
+    tryIO :: String                   -> (String-> Bool)
           -> (String-> State-> State) -> State -> IO State
-    tryIO good change g = do
+    tryIO askUser good change g = do
+      putStrLn askUser
       s <- getLine
       case good s of False -> do putStrLn "No seriously!"
-                                 tryIO good change g
+                                 tryIO askUser good change g
                      True -> wiz $ change s g
 
-    testTryIO = tryIO ((>0) . length . filter (=='_'))
-                      insTplt (empty :: RSLT)
-
-    -- need: something that gets user input, tests against "!q", aborts current command if yes, and proceeds otherwise
-    -- need: a function of type (String -> Bool) -> (State->IO State) -> State -> IO State, where State = (RSLT, CommandStack), which tests the user input with the first arg, and if Bool, 
-
-    insTpltWiz :: RSLT -> IO RSLT
-    insTpltWiz g = do
-      putStrLn "Describe a template."
-      s <- getLine
-      return $ insTplt s g
-
---it works!
--- > wiz $ (empty :: RSLT)
---Command me!
---Id
---Command me!
---InsTplt 
---_ needs _
---Command me!
---Quit
---mkGraph [(0,Tplt ["","needs",""])] []
--- >
+--    insRelWiz :: RSLT -> IO RSLT
+--    insRelWiz g = do
+--      putStrLn "Enter the address of a Template for the new Rel."
+--      n <- (\s->read s::Node) <$> getLine
+--      case gelem n g of False -> do "No, seriously!"; wordWiz g
+--                        True -> 
+--
+    -- tryReadNode :: String -> IO Node
+    -- tryReadNode askUser = ...
