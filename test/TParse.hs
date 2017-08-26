a36e32504d23731278a39551bdda5a64135acfbe
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-08-25 20:25:41 -0700

    test the parser

--- test/TParse.hs
+++ test/TParse.hs
@@ -1,9 +1,9 @@
 module TParse where
 
 import Dwt
-import TData
 import Test.HUnit
 
+import Text.Megaparsec (parse)
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 import Data.Maybe (fromJust)
@@ -27,3 +27,29 @@ tHash = TestCase $ do
     leftConcat (Joint "new") (Leaf "new")
     (RelX (EO True 4) (Leaf "left") (Joint "j") [] (Leaf "right"))
     == RelX (EO True 4) (Leaf "new") (Joint "new") [(Leaf "left",Joint "j")] (Leaf "right")
+  assertBool "4" $ p == Right a where
+      p = parse expr "dogs #like you ###becase you #(give or misplace) (bones #that reek like demons) #to dogs ##sometimes"
+      a = RelX
+          (EO True 3)
+          (RelX
+            (EO True 1)
+            (Leaf "dogs")
+            (Joint "like") []
+            (Leaf "you"))
+          (Joint "becase") []
+          (RelX
+            (EO True 2)
+            (RelX
+              (EO True 1)
+              (Leaf "you")
+              (Joint "give or misplace")
+              [ (RelX
+                  (EO False 1)
+                  (Leaf "bones")
+                  (Joint "that") []
+                  (Leaf "reek like demons")
+                , Joint "to")]
+              (Leaf "dogs"))
+            (Joint "sometimes") []
+            (Leaf ""))
+
