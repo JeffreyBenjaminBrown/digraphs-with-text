3d4871c5ba406b926b3266449b48ee91db0865b7
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-08-26 09:21:55 -0700

    left-justify

--- src/Dwt/Search.hs
+++ src/Dwt/Search.hs
@@ -1,62 +1,62 @@
-    {-# LANGUAGE FlexibleContexts #-}
-    {-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ViewPatterns #-}
 
-    module Dwt.Search (
-      module Dwt.Search
-    ) where
+module Dwt.Search (
+  module Dwt.Search
+) where
 
-    import Text.Regex
+import Text.Regex
 
-    import Dwt.Graph
-    import Data.Graph.Inductive
+import Dwt.Graph
+import Data.Graph.Inductive
 
-    import Data.Map as Map
-    import Data.Maybe as Mb
+import Data.Map as Map
+import Data.Maybe as Mb
 
-    -- see also
-    -- Graph.node :: RSLT -> Expr -> [Node] -- hopefully length = 1
+-- see also
+-- Graph.node :: RSLT -> Expr -> [Node] -- hopefully length = 1
 
-    -- queries
-    data QNode = QNode Node -- when you already know the Node
-      | QWord String | QTplt [String] -- when you don't but you know its contents
-      | QRel QNode [QNode] -- todo ? use
-      deriving (Show, Eq)
+-- queries
+data QNode = QNode Node -- when you already know the Node
+  | QWord String | QTplt [String] -- when you don't but you know its contents
+  | QRel QNode [QNode] -- todo ? use
+  deriving (Show, Eq)
 
-    lengthOne :: [a] -> Either String ()
-    lengthOne ns = do
-      if length ns == 0 then Left "absent" else return ()
-      if length ns > 1 then Left "multiple" else return ()
+lengthOne :: [a] -> Either String ()
+lengthOne ns = do
+  if length ns == 0 then Left "zero matches" else return ()
+  if length ns > 1 then Left "multiple matches" else return ()
 
-    edgeless :: Gr a b -> Gr a b -- ? faster or slower
-    edgeless = gmap (\(_,b,c,_) -> ([], b, c, []))
+edgeless :: Gr a b -> Gr a b -- ? faster or slower
+edgeless = gmap (\(_,b,c,_) -> ([], b, c, []))
 
-    qGet :: RSLT -> QNode -> Either String Node
-    qGet g (QNode n) = gelemM g n >> Right n
-    qGet g (QWord s) = do
-      let ns = nodes $ labfilter (\n -> case n of Word t -> s==t; _ -> False)
-               $ edgeless g
-      lengthOne ns
-      Right $ head ns
-    qGet g (QTplt s) = do
-      let ns = nodes $ labfilter (\n -> case n of Tplt t -> s==t; _ -> False)
-               $ edgeless g
-      lengthOne ns
-      Right $ head ns
+qGet :: RSLT -> QNode -> Either String Node
+qGet g (QNode n) = gelemM g n >> Right n
+qGet g (QWord s) = do
+  let ns = nodes $ labfilter (\n -> case n of Word t -> s==t; _ -> False)
+           $ edgeless g
+  lengthOne ns
+  Right $ head ns
+qGet g (QTplt s) = do
+  let ns = nodes $ labfilter (\n -> case n of Tplt t -> s==t; _ -> False)
+           $ edgeless g
+  lengthOne ns
+  Right $ head ns
 
-    qRegexWord :: RSLT -> String -> Either String [Node] -- why Either?
-    qRegexWord g s = do
-      let r = mkRegex s
-      let ns = nodes $ labfilter 
-                       (\lab -> case lab of Word t -> Mb.isJust $ matchRegex r t;
-                                            _ -> False)
-                       $ edgeless g
-      Right ns
+qRegexWord :: RSLT -> String -> Either String [Node] -- why Either?
+qRegexWord g s = do
+  let r = mkRegex s
+  let ns = nodes $ labfilter (\lab -> case lab of
+                                 Word t -> Mb.isJust $ matchRegex r t;
+                                 _ -> False)
+                   $ edgeless g
+  Right ns
 
-    qInsRel :: QNode -> [QNode] -> RSLT -> Either String RSLT
-    qInsRel qtn qns g = do
-      tn <- qGet g qtn 
-      ns <- mapM (qGet g) qns
-      insRel tn ns g
+qInsRel :: QNode -> [QNode] -> RSLT -> Either String RSLT
+qInsRel qtn qns g = do
+  tn <- qGet g qtn 
+  ns <- mapM (qGet g) qns
+  insRel tn ns g
 
 -- very stale
     --    qGet g (QRel qt qes) = do
