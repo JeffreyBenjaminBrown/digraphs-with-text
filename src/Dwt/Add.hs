8651a80272f514637360a94526657bb255436a2f
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-08-27 16:45:13 -0700

    Translate AddX to Loader

--- src/Dwt/Add.hs
+++ src/Dwt/Add.hs
@@ -1,6 +1,22 @@
 module Dwt.Add where
 
+import Data.Graph.Inductive hiding (empty)
 import Dwt.Graph
-import Dwt.Parse (AddX(..))
+import Dwt.Parse (AddX(..), Level, JointX(..), EO)
 
+import Data.Sequence
 
+-- whereas AddX is optimized for correctness when parsing human-entered data,
+-- Loader is optimized for ease of loading new data into the graph
+data Loader = Loader [Loader]
+            | Joint String
+            | Leaf String
+            | At Node
+            | Absent deriving (Show)
+
+loader :: AddX -> Loader
+loader (LeafX "") = Absent
+loader (LeafX s) = Leaf s
+loader (RelX _ a j more b) = Loader $
+  concatMap f ((a,j) : more) ++ [loader b]
+  where f (a, JointX s) = [loader a, Joint s]
