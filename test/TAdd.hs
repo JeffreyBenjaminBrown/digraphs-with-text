6e4d1ffbd0c78defdc9e91986d351e1a2404cb2c
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-09-08 21:50:01 -0700

    Add: more tests

--- test/TAdd.hs
+++ test/TAdd.hs
@@ -4,19 +4,18 @@ import Dwt hiding (fromRight)
 import Data.Graph.Inductive
 import Test.HUnit hiding (Node)
 import Text.Megaparsec (parse)
-import Control.Monad.Trans.State (runStateT)
+import Control.Monad.Trans.State (runStateT, execStateT)
 
-tAdd = TestList [ TestLabel "tExecAddSt" tExecAddSt ]
-
-tExecAddSt = TestCase $ do
-  let g = snd . fr $ runStateT (addExpr $ fr $ parse expr "" "a #is b") empty
-  assertBool "1" $ g == mkGraph [ (0,Word "a")
-                                , (1,Word "b")
-                                , (2,Tplt ["","is",""])
-                                , (3,Rel)
-                                ]
-                                [ (3,0,RelEdge (Mbr 1))
-                                , (3,1,RelEdge (Mbr 2))
-                                , (3,2,RelEdge TpltRole)
-                                ]
-
+tAdd = TestCase $ do
+  let Right g = execStateT f empty
+      f = mapM (addExpr . fr . parse expr "" ) exprs
+      exprs = ["a #", "# a", "a # b", "## b #"]
+        -- TODO: (a #) and (# a) are indistinguishable
+      qa = QLeaf $ Word "a"
+      qb = QLeaf $ Word "b"
+      qab = QRel (QLeaf $ mkTplt "_ # _") [qa,qb]
+  -- v g $ nodes g -- visual test
+  assertBool "1" $ either (const False) (const True)
+    $ qGet1 g (QLeaf $ Word "a")
+  assertBool "1" $ do
+    either (const False) (const True) $ qGet1 g (QLeaf $ Word "a")
