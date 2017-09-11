f30f9286967ed3a906fca1882d6af8784971a2e1
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-09-10 18:32:05 -0700

    Util: prependCaller -> prependCallerSum Seach: qGet1 -> qGet1Sum        qGetLab -> qGetLabSum        matchRel -> matchRelSum        matchRelLab -> matchRelLabSum        qPutSt -> qPutStSum

--- src/Dwt/Search.hs
+++ src/Dwt/Search.hs
@@ -3,9 +3,9 @@
 
 module Dwt.Search (
     qGet
-  , qGetLab
-  , qGet1
-  , qPutSt
+  , qGetLab, qGetLabSum
+  , qGet1, qGet1Sum 
+  , qPutSt, qPutStSum
   , qRegexWord
 ) where
 
@@ -14,7 +14,7 @@ import Text.Regex
 import Data.Graph.Inductive
 import Dwt.Types
 import Dwt.Graph
-import Dwt.Util (fr, maxNode, dropEdges, fromRight, prependCaller)
+import Dwt.Util (fr, maxNode, dropEdges, fromRight, prependCaller, prependCallerSum)
 import Dwt.Leaf (insLeaf)
 
 import Control.Monad (liftM)
@@ -28,6 +28,7 @@ import Control.Monad (foldM)
 -- TODO: simplify some stuff (maybe outside of this file?) by using 
 -- Graph.whereis :: RSLT -> Expr -> [Node] -- hopefully length = 1
 
+-- ==== The old verbose DwtErr way
 -- == Get
 _qGet :: -- x herein is either Node or LNode Expr
      (RSLT -> Node -> x) -- | gets what's there; used for QAt.
@@ -76,10 +77,56 @@ qPutSt q@(QLeaf x) = get >>= \g -> case qGet1 g q of
     in put g' >> lift (Right $ maxNode g')
   Left e -> lift $ prependCaller "qPutSt: " $ Left e
 
+-- ==== The new terse DwtErrSum way
+_qGetSum :: -- x herein is either Node or LNode Expr
+     (RSLT -> Node -> x) -- | gets what's there; used for QAt.
+  -- Can safely be unsafe, because the QAt's contents are surely present.
+  -> (RSLT -> [x])       -- | nodes or labNodes; used for QLeaf
+  -> (RSLT -> RelSpec -> Either DwtErrSum [x])
+    -- | matchRelSum or matchRelLabSum; used for QRel
+  -> RSLT -> QNode -> Either DwtErrSum [x]
+_qGetSum f _ _ g (QAt n) = return $ if gelem n g then [f g n] else []
+_qGetSum _ f _ g (QLeaf l) = return $ f $ labfilter (==l) $ dropEdges g
+_qGetSum _ _ f g (QRel qt qms) = prependCallerSum "_qGet: " $ do
+  t <- qGet1Sum g qt   -- TODO ? case of multiple qt, qms matches
+  ms <- mapM (qGet1Sum g) qms
+  let relspec = mkRelSpec t ms
+  f g relspec
+
+qGetSum :: RSLT -> QNode -> Either DwtErrSum [Node]
+qGetSum = _qGetSum (\_ n -> n) nodes matchRelSum
+
+qGetLabSum :: RSLT -> QNode -> Either DwtErrSum [LNode Expr]
+qGetLabSum = _qGetSum f labNodes matchRelLabSum where
+  f g n = (n, Mb.fromJust $ lab g n)
+
+qGet1Sum :: RSLT -> QNode -> Either DwtErrSum Node
+qGet1Sum g q = prependCallerSum "qGet1Sum: " $ case qGetSum g q of
+    Right [] -> Left (FoundNo, queryError, ".")
+    Right [a] -> Right a
+    Right as -> Left (FoundMany, queryError, ".")
+    Left e -> Left e
+  where queryError = [ErrQNode q]
+
+qPutStSum :: QNode -> StateT RSLT (Either DwtErrSum) Node
+qPutStSum (QRel qt qms) = do
+  -- TODO ? would be more efficient to return even the half-completed state
+  let tag = prependCallerSum "qPutSt: " -- TODO: use
+  t <- qPutStSum qt
+  ms <- mapM qPutStSum qms
+  g <- get
+  let matches = matchRelSum g $ mkRelSpec t ms
+  insRelStSum t ms
+qPutStSum (QAt n) = lift $ Right n
+qPutStSum q@(QLeaf x) = get >>= \g -> case qGet1Sum g q of
+  Right n -> lift $ Right n
+  Left (FoundNo,_,_) -> let g' = insLeaf x g
+    in put g' >> lift (Right $ maxNode g')
+  Left e -> lift $ prependCallerSum "qPutSt: " $ Left e
+
 -- == Regex
 qRegexWord :: RSLT -> String -> [Node]
 qRegexWord g s = nodes $ labfilter f $ dropEdges g
   where r = mkRegex s
         f (Word t) = Mb.isJust $ matchRegex r t
         f _ = False
-
