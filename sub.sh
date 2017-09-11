dfc1ed679cc84f3ef6b40998e5a26786797e798e
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-09-10 18:44:39 -0700

    use variables for the suffixes for substitution

--- sub.sh
+++ sub.sh
@@ -4,13 +4,18 @@
 # The new one return Either DwtErr.
   # They had the same name plus "De" at the end.
 # This changes names: Give it an argument X, and it changes (order important!)
-  # X to XStrErr
-  # XDe to X
+  # X to XDeprecatoryName
+  # XIntermediateName to X
+# To adjust the values of DeprecatoryName and IntermediateName above,
+  # just change the strings that immediately follow $1 below
 
-find . -name "*.hs" -print0 -o -name "*.txt" -print0 -o -name "*.md" -print0 | xargs -0 sed -i -r "s/([^a-zA-Z0-9\d]|^)$1([^a-zA-Z0-9\d]|$)/\1$1StrErr\2/g"
+DeprecatoryName=LongErr # the old way is renamed with this suffix
+IntermediateName=Sum # the new way has this suffix removed
 
-find . -name "*.hs" -print0 -o -name "*.txt" -print0 -o -name "*.md" -print0 | xargs -0 sed -i -r "s/([^a-zA-Z0-9\d]|^)$1De([^a-zA-Z0-9\d]|$)/\1$1\2/g"
+find . -name "*.hs" -print0 -o -name "*.txt" -print0 -o -name "*.md" -print0 | xargs -0 sed -i -r "s/([^a-zA-Z0-9\d]|^)$1([^a-zA-Z0-9\d]|$)/\1$1DeprecatoryName\2/g"
+
+find . -name "*.hs" -print0 -o -name "*.txt" -print0 -o -name "*.md" -print0 | xargs -0 sed -i -r "s/([^a-zA-Z0-9\d]|^)$1IntermediateName([^a-zA-Z0-9\d]|$)/\1$1\2/g"
 
 echo "Here's a handy commit message:"
-echo "change names: $1De -> $1 -> $1StrErr"
+echo "change names: $1IntermediateName -> $1 -> $1DeprecatoryName"
 echo "If the function name is a common English word, or if it might have been used in comments, don't forget to check the diff!"
