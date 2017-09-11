045296c886f6eff8977de9e74906a464fc6632f8
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-09-11 16:54:41 -0700

    fork1Dir: clarify names

--- src/Dwt/Graph.hs
+++ src/Dwt/Graph.hs
@@ -264,16 +264,17 @@ otherDir Up = Down
 otherDir Down = Up
 
 fork1Dir :: RSLT -> Node -> (Mbrship,RelSpec) -> Either DwtErr [Node]
-fork1Dir g n (dir,r) = do -- returns one generation, neighbors
-  if has1Dir (otherDir dir) r then return ()
-     else Left (Invalid,  [ErrRelSpec r]
-               , "fork1DirSum: should have only one " ++ show (otherDir dir))
-  let r' = subNodeForVars n (otherDir dir) r
-      dirRoles = Map.keys $ Map.filter (== VarSpec dir) r
-  rels <- matchRelSpecNodes g r'
+fork1Dir g from (dir,axis) = do -- returns one generation, neighbors
+  let fromDir = otherDir dir
+  if has1Dir fromDir axis then return ()
+     else Left (Invalid,  [ErrRelSpec axis]
+               , "fork1DirSum: should have only one " ++ show fromDir)
+  let axis' = subNodeForVars from fromDir axis
+      dirRoles = Map.keys $ Map.filter (== VarSpec dir) axis
+  rels <- matchRelSpecNodes g axis'
   concat <$> mapM (\rel -> relElts g rel dirRoles) rels
     -- TODO: this line is unnecessary. just return the rels, not their elts.
-    -- EXCEPT: that might hurt the dfs, bfs functions below
+      -- EXCEPT: that might hurt the dfs, bfs functions below
 
 fork1Dirs :: RSLT -> Node -> [(Mbrship,RelSpec)] -> Either DwtErr [Node]
 fork1Dirs g n rs = concat <$> mapM (fork1Dir g n) rs
