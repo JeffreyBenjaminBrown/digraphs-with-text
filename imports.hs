d51dee2a154a0349b615659c381a5dec038da411
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-09-10 23:07:30 -0700

    drop unused imports

--- imports.hs
+++ imports.hs
@@ -1,20 +1,11 @@
-:module -Dwt.ParsecUtils 
-
-import Text.Megaparsec
 import Control.Monad
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.State
 
-import qualified Text.Parsec as P
-import qualified Text.Parsec.String as P
-import qualified Dwt.ParsecUtils as P
-
-import qualified Text.Read as R
-import qualified Data.Text as T
-
 import qualified Data.List as L
 import qualified Data.Map as M
 import qualified Data.Maybe as Mb
+import qualified Text.Megaparsec as Mp
 import qualified Brick.Main as B
 
 :set prompt ">>> "
