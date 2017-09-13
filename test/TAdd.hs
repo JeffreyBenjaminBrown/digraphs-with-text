c250f65a90079ac424ef46aab97830daf42d1b8d
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-09-13 10:48:09 -0700

    restore either to Dwt.cabal   I use it in the repl, even if not in the code TAdd: test unlabeled and labeled queries separately

--- test/TAdd.hs
+++ test/TAdd.hs
@@ -6,15 +6,29 @@ import Test.HUnit hiding (Node)
 import Text.Megaparsec (parse)
 import Control.Monad.Trans.State (runStateT, execStateT)
 
-tAdd = TestCase $ do
+tAdd = TestList [ TestLabel "tAddLabeled" tAddLabeled
+                , TestLabel "tAddUnlabeled" tAddUnlabeled
+                ]
+
+tAddLabeled = TestCase $ do
+  let Right g = execStateT f empty
+      f = mapM (addExpr . fr . parse expr "" ) exprs
+      exprs = ["a #x", "#x a", "a #x b", "##x b #x"]
+      qa = QLeaf $ Word "a"
+      qb = QLeaf $ Word "b"
+      qab = QRel (QLeaf $ mkTplt "_ x _") [qa,qb]
+  assertBool "1" $ either (const False) (const True) $ qGet1 g qa
+  assertBool "2" $ do either (const False) (const True) $ qGet1 g qb
+  assertBool "3" $ do either (const False) (const True) $ qGet1 g qab
+
+tAddUnlabeled = TestCase $ do
   let Right g = execStateT f empty
       f = mapM (addExpr . fr . parse expr "" ) exprs
       exprs = ["a #", "# a", "a # b", "## b #"]
-        -- TODO: (a #) and (# a) are indistinguishable
+        -- TODO: unlabeled rels (a #) and (# a) are visually indistinguishable
       qa = QLeaf $ Word "a"
       qb = QLeaf $ Word "b"
-      qab = QRel (QLeaf $ mkTplt "_ # _") [qa,qb]
-  -- v g $ nodes g -- visual test
+      qab = QRel (QLeaf $ mkTplt "_ _") [qa,qb]
   assertBool "1" $ either (const False) (const True) $ qGet1 g qa
   assertBool "2" $ do either (const False) (const True) $ qGet1 g qb
   assertBool "3" $ do either (const False) (const True) $ qGet1 g qab
