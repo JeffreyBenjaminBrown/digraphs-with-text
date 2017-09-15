809a435513ba51d82174499ad05dadc80bfaaaa5
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-09-14 21:34:02 -0700

    parse an instruction to return all nodes in the graph

--- src/Dwt/Search/Parse.hs
+++ src/Dwt/Search/Parse.hs
@@ -1,6 +1,6 @@
 module Dwt.Search.Parse where
 
-import Data.Graph.Inductive (Node, pre)
+import Data.Graph.Inductive (Node, pre, nodes)
 import Dwt.Types
 import Dwt.ParseUtils (Parser, integer, symbol)
 import Text.Megaparsec.Char (satisfy, string, char)
@@ -15,3 +15,6 @@ pUsers :: Parser (Reader RSLT [Node])
 pUsers = f <$> (symbol "u" *> integer) where
   f insertion = do g <- ask
                    return $ pre g insertion
+
+pAll :: Parser (Reader RSLT [Node])
+pAll = const (asks nodes) <$> symbol "all"
