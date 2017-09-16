2e4d24300e4df7d28b788e1e3d9fec234286fa4b
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-09-15 19:13:22 -0700

    UI: shuffle for navigability

--- src/Dwt/UI.hs
+++ src/Dwt/UI.hs
@@ -10,31 +10,26 @@ import Dwt.Hash.Insert (addExpr)
 import Dwt.Hash.Parse
 import Dwt.Search.Parse
 import Dwt.Util (fr)
-import Text.Megaparsec (parse, (<|>))
-import Control.Monad.Trans.State (execStateT)
-import Control.Monad.Trans.Reader (runReader)
 
+import Brick.Widgets.Core ( (<+>), (<=>), hLimit, vLimit, str)
+import Brick.Util (on)
 import qualified Brick.Main as M
 import qualified Brick.Types as T
-import Brick.Widgets.Core
-  ( (<+>)
-  , (<=>)
-  , hLimit
-  , vLimit
-  , str
-  )
 import qualified Brick.Widgets.Center as C
 import qualified Brick.Widgets.Edit as E
 import qualified Brick.AttrMap as A
 import qualified Brick.Focus as F
-import Brick.Util (on)
 
-import Lens.Micro
-import Lens.Micro.TH
 import qualified Graphics.Vty as V
 import qualified Data.Text.Zipper as Z
+import Lens.Micro
+import Lens.Micro.TH
 
+import Text.Megaparsec (parse, (<|>))
+import Control.Monad.Trans.State (execStateT)
+import Control.Monad.Trans.Reader (runReader)
 import Data.List (partition)
+
 
 data Name = Edit1 | Edit2 deriving (Ord, Show, Eq)
 
@@ -42,12 +37,59 @@ data St = St { _rslt :: RSLT
              , _nodesInView :: [Node]
              , _viewRequests :: [String]
              ,_focusRing :: F.FocusRing Name
-             , _edit1 :: E.Editor String Name
-             , _edit2 :: E.Editor String Name
+             , _edit1, _edit2 :: E.Editor String Name
              }
 
 makeLenses ''St
 
+initialState :: RSLT -> St
+initialState g = St g (nodes g) [] (F.focusRing [Edit1, Edit2])
+  (E.editor Edit1 Nothing "") (E.editor Edit2 (Just 2) "")
+
+
+-- ==== Change state
+viewRSLT :: St -> T.EventM Name (T.Next St)
+viewRSLT st = do
+    let (request:_) = st ^. edit2 & E.getEditContents
+          -- TODO: find a more elegant way to take only one string
+        theReader = fr $ parse (pUsers <|> pAll) "" request -- TODO: nix fr
+        newNodesInView = runReader theReader $ st ^. rslt
+        f1 = nodesInView .~ newNodesInView
+        f2 = edit2 %~ E.applyEdit Z.clearZipper
+    M.continue $ st & f2 . f1
+
+addToRSLT :: St -> T.EventM Name (T.Next St)
+addToRSLT st = do
+    let strings = st ^. edit1 & E.getEditContents
+        graphUpdater = mapM (addExpr . fr . parse expr "" ) strings
+          -- TODO: nix the fr
+        g = st ^. rslt
+        e = execStateT graphUpdater g
+    let f1 = edit1 %~ E.applyEdit Z.clearZipper
+        f2 = case e of Left _ -> id -- TODO: display the error
+                       Right g' -> rslt .~ g'
+    M.continue $ st & f2 . f1
+
+
+-- ==== Controls
+appHandleEvent :: St -> T.BrickEvent Name e -> T.EventM Name (T.Next St)
+appHandleEvent st (T.VtyEvent ev) = case ev of
+  V.EvKey V.KEsc [] -> M.halt st
+  V.EvKey V.KIns [] -> addToRSLT st
+  V.EvKey V.KHome [] -> viewRSLT st
+  V.EvKey (V.KChar '\t') [] -> M.continue $ st & focusRing %~ F.focusNext
+  V.EvKey V.KBackTab [] -> M.continue $ st & focusRing %~ F.focusPrev
+  _ -> M.continue =<< case F.focusGetCurrent (st^.focusRing) of
+    Just Edit1 -> T.handleEventLensed st edit1 E.handleEditorEvent ev
+    Just Edit2 -> T.handleEventLensed st edit2 E.handleEditorEvent ev
+    Nothing -> return st
+appHandleEvent st _ = M.continue st
+
+appChooseCursor :: St -> [T.CursorLocation Name] -> Maybe (T.CursorLocation Name)
+appChooseCursor = F.focusRingCursor (^.focusRing)
+
+
+-- ==== Draw
 appDraw :: St -> [T.Widget Name]
 appDraw st = [ui] where
   g = st ^. rslt
@@ -72,52 +114,10 @@ appDraw st = [ui] where
     <=> str " "
     <=> str "Press Tab to switch between editors, Esc to quit."
 
-appHandleEvent :: St -> T.BrickEvent Name e -> T.EventM Name (T.Next St)
-appHandleEvent st (T.VtyEvent ev) = case ev of
-  V.EvKey V.KEsc [] -> M.halt st
-  V.EvKey V.KIns [] -> addToRSLT st
-  V.EvKey V.KHome [] -> viewRSLT st
-  V.EvKey (V.KChar '\t') [] -> M.continue $ st & focusRing %~ F.focusNext
-  V.EvKey V.KBackTab [] -> M.continue $ st & focusRing %~ F.focusPrev
-  _ -> M.continue =<< case F.focusGetCurrent (st^.focusRing) of
-    Just Edit1 -> T.handleEventLensed st edit1 E.handleEditorEvent ev
-    Just Edit2 -> T.handleEventLensed st edit2 E.handleEditorEvent ev
-    Nothing -> return st
-appHandleEvent st _ = M.continue st
-
-viewRSLT :: St -> T.EventM Name (T.Next St)
-viewRSLT st = do
-    let (request:_) = st ^. edit2 & E.getEditContents
-          -- TODO: find a more elegant way to take only one string
-        theReader = fr $ parse (pUsers <|> pAll) "" request -- TODO: nix fr
-        newNodesInView = runReader theReader $ st ^. rslt
-        f1 = nodesInView .~ newNodesInView
-        f2 = edit2 %~ E.applyEdit Z.clearZipper
-    M.continue $ st & f2 . f1
-
-addToRSLT :: St -> T.EventM Name (T.Next St)
-addToRSLT st = do
-    let strings = st ^. edit1 & E.getEditContents
-        graphUpdater = mapM (addExpr . fr . parse expr "" ) strings
-          -- TODO: nix the fr
-        g = st ^. rslt
-        e = execStateT graphUpdater g
-    let f1 = edit1 %~ E.applyEdit Z.clearZipper
-        f2 = case e of Left _ -> id -- TODO: display the error
-                       Right g' -> rslt .~ g'
-    M.continue $ st & f2 . f1
-
-initialState :: RSLT -> St
-initialState g = St g (nodes g) [] (F.focusRing [Edit1, Edit2])
-  (E.editor Edit1 Nothing "") (E.editor Edit2 (Just 2) "")
-
 appAttrMap :: A.AttrMap
 appAttrMap = A.attrMap V.defAttr [ (E.editAttr       , V.white `on` V.blue)
                                  , (E.editFocusedAttr, V.black `on` V.yellow)
                                  ]
-
-appChooseCursor :: St -> [T.CursorLocation Name] -> Maybe (T.CursorLocation Name)
-appChooseCursor = F.focusRingCursor (^.focusRing)
 
 theApp :: M.App St e Name
 theApp = M.App { M.appDraw = appDraw
@@ -127,6 +127,8 @@ theApp = M.App { M.appDraw = appDraw
                , M.appAttrMap = const appAttrMap
                }
 
+
+-- ==== Main
 ui :: RSLT -> IO (RSLT)
 ui g = do st <- M.defaultMain theApp $ initialState g
           return $ st ^. rslt
