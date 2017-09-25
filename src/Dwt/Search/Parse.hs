1a3af38352e29245496921060fc36e9a03d2eeec
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-09-24 20:38:49 -0700

    UI: allow queries involving QVars

--- src/Dwt/Search/Parse.hs
+++ src/Dwt/Search/Parse.hs
@@ -3,6 +3,7 @@ module Dwt.Search.Parse where
 import Data.Graph.Inductive (Node, pre, nodes)
 import Dwt.Types
 import Dwt.ParseUtils (Parser, integer, symbol, word)
+import Text.Megaparsec (try)
 import Dwt.Hash.Parse (expr)
 import Dwt.Search.QNode (qGet)
 import Control.Applicative ((<|>))
@@ -13,23 +14,24 @@ data Command = ViewGraph ReadNodes | ShowQueries
 type ReadNodes = Reader RSLT (Either DwtErr [Node])
 
 pCommand :: Parser Command
-pCommand = foldl1 (<|>) [pUsers, pAllNodes, pShowQueries, pQNode]
+pCommand = foldl1 (<|>) $ map try [pUsers, pAllNodes, pShowQueries, pQNode]
 
 pQNode :: Parser Command
-pQNode = ViewGraph . f <$> (word "qn" >> expr) where
+pQNode = ViewGraph . f <$> expr where
   f qnode = do g <- ask
                return $ qGet g qnode
 
 pUsers :: Parser Command
-pUsers = ViewGraph . f <$> (symbol "u" *> integer) where
+pUsers = ViewGraph . f <$> (symbol "@u" *> integer) where
   f node = do g <- ask
               return $ Right $ pre g node
   -- TODO: what if qNode is not present? use QNode, not Node
+  -- or better: TODO: expand QNode to include stars
 
 pAllNodes :: Parser Command
-pAllNodes = const f <$> symbol "all" where
+pAllNodes = const f <$> symbol "@all" where
    f = ViewGraph $ do g <- ask
                       return $ Right $ nodes g
 
 pShowQueries :: Parser Command
-pShowQueries = const ShowQueries <$> symbol "qs"
+pShowQueries = const ShowQueries <$> symbol "@queries"
